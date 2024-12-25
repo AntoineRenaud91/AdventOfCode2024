3bc80899fdfde1a4560c9336fc9b48ef1688a936
= <=>
2024-12-25 11:58:22 +0100

    day25

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,5 +1,7 @@
-use std::{collections::{BTreeMap, BTreeSet}, path::PathBuf};
-
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    path::PathBuf,
+};
 
 #[cfg(test)]
 const TEST_INPUT: &str = "
@@ -37,31 +39,34 @@ tb-vc
 td-yn
 ";
 
-
-fn parse(input: &str) -> BTreeMap<&str, BTreeSet<&str>>  {
-    input.trim().lines().fold(BTreeMap::<&str,BTreeSet<&str>>::default(),|mut map, line| {
-        let (a,b) = line.split_once('-').unwrap();
-        map.entry(a).or_default().insert(b);
-        map.entry(b).or_default().insert(a);
-        map
-    })
+fn parse(input: &str) -> BTreeMap<&str, BTreeSet<&str>> {
+    input.trim().lines().fold(
+        BTreeMap::<&str, BTreeSet<&str>>::default(),
+        |mut map, line| {
+            let (a, b) = line.split_once('-').unwrap();
+            map.entry(a).or_default().insert(b);
+            map.entry(b).or_default().insert(a);
+            map
+        },
+    )
 }
 
 fn process1(input: &str) -> usize {
     let adjs = parse(input);
     let mut groups = vec![];
-    for (k1,set2) in adjs.iter() {
+    for (k1, set2) in adjs.iter() {
         for k2 in set2.iter().rev().take_while(|k| k > &k1) {
             for k3 in adjs.get(k2).unwrap().iter().rev().take_while(|k| k > &k2) {
                 if adjs.get(k3).unwrap().contains(k1) {
-                    groups.push(vec![k1,k2,k3]);
+                    groups.push(vec![k1, k2, k3]);
                 }
             }
         }
     }
-    groups.iter().filter(|group| {
-        group.iter().any(|k| k.starts_with("t"))
-    }).count()
+    groups
+        .iter()
+        .filter(|group| group.iter().any(|k| k.starts_with("t")))
+        .count()
 }
 
 #[test]
@@ -74,28 +79,26 @@ fn process2(input: &str) -> String {
     let mut largest = BTreeSet::default();
     'outer: for (k1, nexts) in adjs.iter() {
         if nexts.len() < largest.len() {
-            continue
+            continue;
         }
         let mut set = BTreeSet::from([*k1]);
-        for (i,next) in nexts.iter().enumerate() {
+        for (i, next) in nexts.iter().enumerate() {
             if set.is_subset(adjs.get(next).unwrap()) {
                 set.insert(next);
             }
-            if set.len()+nexts.len()-i <= largest.len() {
-                continue 'outer
-            } 
+            if set.len() + nexts.len() - i <= largest.len() {
+                continue 'outer;
+            }
         }
         largest = set;
     }
     largest.iter().cloned().collect::<Vec<&str>>().join(",")
 }
 
-
 #[test]
 fn test_process2() {
     assert_eq!(process2(TEST_INPUT), "co,de,ka,ta")
 }
-
 
 fn main() {
     let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
@@ -105,8 +108,8 @@ fn main() {
     let input = std::fs::read_to_string(path).unwrap();
     let start = std::time::Instant::now();
     let result = process1(&input);
-    println!("Result part 1: {result} in {:?}",start.elapsed());
+    println!("Result part 1: {result} in {:?}", start.elapsed());
     let start = std::time::Instant::now();
     let result = process2(&input);
-    println!("Result part 2: {result} in {:?}",start.elapsed());
+    println!("Result part 2: {result} in {:?}", start.elapsed());
 }
